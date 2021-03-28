2a373806d5bedb696f9862f95eaee2d629a72577
Chris Caudill <caudilci@plu.edu>
2021-03-28 15:03:59 -0700

    updated with shortcuts to get entities and intent

--- helpers/luis_helper.py
+++ helpers/luis_helper.py
@@ -36,16 +36,16 @@ class LuisHelper:
 
         try:
             recognizer_result = await luis_recognizer.recognize(turn_context)
-
-            intent = (
-                sorted(
-                    recognizer_result.intents,
-                    key=recognizer_result.intents.get,
-                    reverse=True,
-                )[:1][0]
-                if recognizer_result.intents
-                else None
-            )
+            intent = list(recognizer_result.intents.keys())[0]
+            # intent = (
+            #     sorted(
+            #         recognizer_result.intents,
+            #         key=recognizer_result.intents.get,
+            #         reverse=True,
+            #     )[:1][0]
+            #     if recognizer_result.intents
+            #     else None
+            # )
 
             if intent == Intent.RESTAURANT.value:
                 result = RestaurantQuery()
@@ -55,23 +55,17 @@ class LuisHelper:
                     "food", []
                 )
                 if len(food_entities) > 0:
-                    if recognizer_result.entities.get("food", [{"$instance": {}}])[0][
-                        "$instance"
-                    ]:
-                        result.food = to_entities[0]["text"].capitalize()
-                    else:
-                        result.food = None
+                    result.food = food_entities[0]["text"]
+                else:
+                    result.food = None
 
-                from_entities = recognizer_result.entities.get("$instance", {}).get(
+                loc_entities = recognizer_result.entities.get("$instance", {}).get(
                     "location", []
                 )
-                if len(from_entities) > 0:
-                    if recognizer_result.entities.get("location", [{"$instance": {}}])[0][
-                        "$instance"
-                    ]:
-                        result.loc = from_entities[0]["text"].capitalize()
-                    else:
-                        result.loc = None
+                if len(loc_entities) > 0:
+                    result.loc = loc_entities[0]["text"].capitalize()
+                else:
+                    result.loc = None
 
         except Exception as exception:
             print(exception)
